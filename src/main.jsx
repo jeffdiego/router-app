115996ae14ddd74410b1d32ba3b7b643433df233
Angelo <angelolustosa@gmail.com>
2025-09-02 21:07:27 -0300

    Configurando a rota com o 404

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Login } from './pages/Login.jsx';
+import { Cadastro } from './pages/Cadastro.jsx';
+import { NotFound } from './pages/NotFound.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      {/* <App /> */}
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/home' element={<App />} />
+        <Route path='/cadastro' element={<Cadastro />} />
+        {/* 404 */}
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
