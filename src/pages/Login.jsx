d8a8c985136ebc4ab28266049196bc8eaaf6830d
JEFFERSON DIEGO LIMA DA SILVA <jeffersonsilva0425@digitalcollege.internal>
2025-09-02 22:02:17 -0300

    Continuando

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,36 @@
+import { useNavigate } from "react-router-dom";
+
 export const Login = () => {
+    const banana = useNavigate();
+
+const login = e => {
+    e.preventDefault();
+console.log('logou');
+banana('/home')
+}
+
   return (
-    <div>Login</div>
+   <main className="form-signin w-100 m-auto">
+<img className="mb-4" src="https://getbootstrap.com/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57" />
+            <form onSubmit={login}>
+                <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+                <div className="form-floating">
+                    <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                    <label htmlFor="floatingInput">Email address</label>
+                </div>
+                <div className="form-floating">
+                    <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                    <label htmlFor="floatingPassword">Password</label>
+                </div>
+                <div className="form-check text-start my-3">
+                    <input className="form-check-input" type="checkbox" value="remember-me" id="checkDefault" />
+                    <label className="form-check-label" htmlFor="checkDefault">
+                        Remember me
+                    </label>
+                </div>
+                <button className="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+                <p className="mt-5 mb-3 text-body-secondary">© 2017–2025</p>
+            </form>
+        </main>
   )
 }
