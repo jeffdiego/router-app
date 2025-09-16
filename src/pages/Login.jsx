2fe63322cb3aa9114cc8b0b370f63bbb62d99cc8
JEFFERSON DIEGO LIMA DA SILVA <jeffersonsilva0425@digitalcollege.internal>
2025-09-16 20:21:54 -0300

    Concluindo Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,89 +4,57 @@ import { InputField } from "../components/input/InputField";
 import { useState } from "react";
 
 export const Login = () => {
-    const senha = ''
-    const navigate = useNavigate();
-    const [formData, setFormData] = useState({
-        email: '',
-        senha: ''
-    });
+  const senha = '';
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState();
 
-    const login = e => {
-        /* evita recarregar a página */
-        e.preventDefault();
-        console.log('dados', formData)
-        const { email, password } = formData;
-        if (email === 'angelo@teste.com' && password === '123') {
-            alert('Autenticação bem sucedida!')
-            console.log('logou', JSON.stringify(formData, null, 2));
-            navigate('/home');
-        } else {
-            alert('Usuário ou senha incorreto!')
-        }
-    }
+  const login = (e) => {
+    /* evita recarregar a página */
+    e.preventDefault();
+    console.log("logou");
+    navigate("/home");
+  };
 
-    // Handle change genérico
-    const handleChange = e => {
-        const { name, type, value, checked } = e.target;
-        setFormData(prev => ({
-            ...prev,
-            [name]: type === "checkbox" ? checked : value
-        }));
-    };
+  return (
+    <main className="form-signin w-100 m-auto">
+      <img
+        className="mb-4"
+        src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo.svg"
+        alt=""
+        width="72"
+        height="57"
+      />
+      <form onSubmit={login}>
+        <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+        <InputField
+          id="email"
+          label="Email"
+          type="email"
+          placeholder="name@exemple.com"
+        />
+        <InputField
+          id="senha"
+          label="Senha"
+          type="password"
+          placeholder="senha"
+        />
 
-    // const handleEmail = (e) => {
-    //     console.log(e.target.id, e.target.value)
-
-    //     setFormData({
-    //         email: e.target.value
-    //     })
-    // }
-
-    const handleInput = (e) => {
-        console.log(e.target.id, e.target.value)
-
-        setFormData({
-            ...formData,
-            [e.target.id]: e.target.value
-        })
-    }
-
-    return (
-        <main className="form-signin w-100 m-auto">
-            <img className="mb-4" src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57" />
-            <form onSubmit={login}>
-                <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
-
-                <InputField
-                    id='email'
-                    label='Email'
-                    type='email'
-                    placeholder="name@example.com"
-                    value={formData.email}
-                    onChange={handleInput}
-                />
-
-                <InputField
-                    id='senha'
-                    label='Senha'
-                    type='password'
-                    placeholder="senha"
-                    value={formData.senha}
-                    onChange={handleInput}
-
-
-                />
-
-
-                <div className="form-check text-start my-3">
-                    <input className="form-check-input" type="checkbox" value="remember-me" id="checkDefault" />
-                    <label className="form-check-label" htmlFor="checkDefault">
-                        Remember me
-                    </label>
-                </div>
-                <button className="btn btn-success w-100 py-2" type="submit">Entrar</button>
-                <p className="mt-5 mb-3 text-body-secondary">© 2017–2025</p>
-            </form>
-        </main>
-    )
-}
+        <div className="form-check text-start my-3">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value="remember-me"
+            id="checkDefault"
+          />
+          <label className="form-check-label" htmlFor="checkDefault">
+            Remember me
+          </label>
+        </div>
+        <button className="btn btn-primary w-100 py-2" type="submit">
+          Sign in
+        </button>
+        <p className="mt-5 mb-3 text-body-secondary">© 2017–2025</p>
+      </form>
+    </main>
+  );
+};
